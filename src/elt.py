ebb6da4a9e218713c391be9d668e2e754b23e548
GenivaldoSerra <genivaldo.serra@live.com>
2024-06-14 10:48:51 -0300

    feat: Adicionando funcionalidade de extração e carregamento no postgresql

---
+++ src/elt.py
@@ -0,0 +1,41 @@
+import yfinance as yf
+import pandas as pd
+import os
+
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+load_dotenv()
+
+commodities = ['GC=F', 'SI=F', 'CL=F', 'NG=F']
+
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PRO')
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+
+def buscar_dados(simbolo, periodo='5d', intervalo='1d'):
+    ticker = yf.Ticker(simbolo)
+    dados = ticker.history(period=periodo, interval=intervalo)[['Close']]
+    dados['simbolo'] = simbolo
+    return dados
+
+def todos_dados(commodities):
+    todos_dados = []
+    for s in commodities:
+        dados = buscar_dados(s)
+        todos_dados.append(dados)
+    return pd.concat(todos_dados)
+
+def salvar_postgres(df, schema='public'):
+    df.to_sql('commodities', engine, if_exists='replace', index=True, index_label='Date', schema=schema)
+
+if __name__ == '__main__':
+    dados = todos_dados(commodities)
+    salvar_postgres(dados, schema='public')
